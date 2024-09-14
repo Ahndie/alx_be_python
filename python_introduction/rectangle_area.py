f8bd6cb8fb2ab286b8495ad3b9f1015066d03610
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-09-14 03:09:58 +0100

    area calculated

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,15 @@
+def calculate_area_of_rectangle(length, width):
+    
+    # Calculate the area
+    area = length * width
+    return area
+
+# Fixed dimensions
+length = 10
+width = 5
+
+# Calculate the area
+area = calculate_area_of_rectangle(length, width)
+
+# Print the area
+print(f"The area of the rectangle is: {int(area)}") 
