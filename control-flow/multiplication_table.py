86d3fec22c683fcae01762acbe9266db9bdec050
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-09-21 17:13:13 +0100

    multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+# using for loop to generate a multiplication times table
+
+number = int(input("Enter a number to see its multiplication table:"))
+for i in range(1,11):
+	result = number * i 
+	print(f"{number} x {i} = {result}") 
