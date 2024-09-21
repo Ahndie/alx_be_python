cd8529bec03392f025b140d8e42c57f8bc0392ce
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-09-21 19:41:11 +0100

    update

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,16 +1,18 @@
-# Prompt the user for a positive integer
-size = int(input("Enter the size of the pattern: "))
-
+size = int(input("Enter the size of the pattern:" ))
+   
 # Ensure the user enters a positive integer
 if size > 0:
-    # Outer loop for the rows
-    for i in range(size):
-        # Inner loop for the columns
-        for j in range(size):
-            print("*", end=" ")
-        # Move to the next line after each row
-        print()
-else:
-    print("Please enter a positive integer.")
-
-
+    row = 0
+    # Outer while loop for the rows
+    while row < size:
+    # Initialize column counter
+    column = 0
+        
+    # Inner while loop for columns
+    while column < size:
+        print("*", end= " ")
+        column += 1
+         
+    # Move to the next line after each row
+    print()
+    row += 1
