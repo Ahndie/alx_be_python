9f75a1b4b2370867592df5d40958946fed3884d0
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-10-02 16:37:18 +0100

    update

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,8 @@
 # temp_conversion_tool.py
 
 #Global Conversion Factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # Global Factor to convert Fahrenheit to Celsius
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Global Factor to convert Celsius to Fahrenheit
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)  # Global Factor to convert Fahrenheit to Celsius
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)  # Global Factor to convert Celsius to Fahrenheit
 
 
 def convert_to_celsius(fahrenheit): #Convert Fahrenheit to Celsius using the global conversion factor.
