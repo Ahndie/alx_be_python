ffdbda37451a3dc7a8b46532ea7951a4549d164a
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-10-13 13:20:13 +0100

    update

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,7 +11,7 @@ class Book:
     	return f"Book('{self.title}', '{self.author}', {self.year})"
  
     def __del__(self):
-        print(f"Deleting {self.title}.")	
+        print(f"Deleting {self.title}")	
 
 # Creating an instance of book
 book = Book('1984', 'George Orwell', 1949)
