0b28a3d12c1fa9f22ccd620d4bb7759e919a2741
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-10-13 12:52:02 +0100

    update

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,12 +2,12 @@ class Book:
     def __init__(self, title, author, publication_year):
         self.title = title
         self.author = author
-        self.publication_year = publication year
+        self.year = year
 
     def __str__(self):
-    	return f"{self.title} by {self.author}, published in {self.publication_year}"
+    	return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-    	return f"Book(title={self.title}, author={self.author}, publication_year={self.publication_year})"
-    	
+    	return f"Book(title={self.title}, author={self.author}, year={self.year})"
+
     	
