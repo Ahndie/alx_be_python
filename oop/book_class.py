4eef8cda8c7170ab0d96fabdd4c8d4aa1b94114a
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-10-13 23:01:34 +0100

    update

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,20 @@
-class Book: 
+# book_class.py
+
+class Book:
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
+    def __del__(self):
+        print(f"Deleting '{self.title}'")
+
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"  
+        return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    def __del__(self):
-        print(f"Deleting {self.title}")  # Adjusted to remove quotes around the title
-
-# Creating an instance of book
-book = Book('1984', 'George Orwell', 1949)
+if __name__ == "__main__":
+    # Creating a book instance
+    my_book = Book("1984", "George Orwell", 1949)
