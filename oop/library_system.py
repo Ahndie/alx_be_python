b51eeed9cc2e3006b715ae556346d76f92189c2d
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-10-19 20:27:10 +0100

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,27 +1,26 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.year = year
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
-    def __init__(self, title, author, year, file_size):
-        super().__init__(title, author, year)
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}, published in {self.year} (EBook, {self.file_size} KB)"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
-    def __init__(self, title, author, year, page_count):
-        super().__init__(title, author, year)
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}, published in {self.year} (PrintBook, {self.page_count} pages)"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -31,8 +30,5 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        if not self.books:
-            print("The library is empty.")
-        else:
-            for book in self.books:
-                print(book)
+        for book in self.books:
+            print(book)
